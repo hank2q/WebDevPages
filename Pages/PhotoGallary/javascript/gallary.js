98e2dc918189c8c23a1f0988227e71eda3c82c2a
Hank <hassanien.kaissi@gmail.com>
2020-08-05 17:35:35 +0400

    made preload image functionality in js

--- Pages/PhotoGallary/javascript/gallary.js
+++ Pages/PhotoGallary/javascript/gallary.js
@@ -9,7 +9,18 @@ function main(event) {
     var container = document.querySelector(".container");
     var image = document.querySelector(".image");
     var lable = document.querySelector(".lable");
-    image.src = files[0];
+    var images = [];
+    (function () {
+        for (img in files) {
+            images[img] = new Image();
+            images[img].src = files[img];
+            images[img].className = "image";
+            images[img].alt = "Photo";
+        }
+    })();
+    // image = images[0];
+    image.parentNode.replaceChild(images[0], image);
+    image = document.querySelector(".image");
     lable.textContent = names[0];
     var allBtn = document.querySelector(".all");
     var next = document.querySelector(".next");
@@ -18,18 +29,20 @@ function main(event) {
 
     function Next() {
         current += 1;
-        if (current == files.length) {
+        if (current == images.length) {
             current = 0;
         }
-        image.src = files[current];
+        image = document.querySelector(".image");
+        image.parentNode.replaceChild(images[current], image);
         lable.textContent = names[current];
     }
     function Prev() {
         current -= 1;
         if (current < 0) {
-            current = files.length - 1;
+            current = images.length - 1;
         }
-        image.src = files[current];
+        image = document.querySelector(".image");
+        image.parentNode.replaceChild(images[current], image);
         lable.textContent = names[current];
     }
     document.addEventListener("keyup", function (event) {
