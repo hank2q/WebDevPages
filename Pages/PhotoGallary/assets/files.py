b852871aa459b47ad6ef2b181820c0faa2b79094
Hank <hassanien.kaissi@gmail.com>
2020-08-07 07:45:00 +0400

    rearranged files

--- Pages/PhotoGallary/assets/files.py
+++ Pages/PhotoGallary/assets/files.py
@@ -3,6 +3,6 @@ import os
 images = ['images/' + image for image in os.listdir('../images')]
 images.sort()
 names = [os.path.splitext(os.path.basename(name))[0] for name in images]
-with open('../javascript/files.js', 'w') as f:
+with open('files.js', 'w') as f:
     f.write(f'var files = {images}\n')
     f.write(f'var names = {names}')
